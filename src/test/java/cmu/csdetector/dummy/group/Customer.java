0b03627dc2c602f60f8237e5509c718ed6e7c848
Saksham Bhalla <bhallasaksham@gmail.com>
2023-04-03 16:26:33 -0700

    chore: add test files

---
+++ src/test/java/cmu/csdetector/dummy/group/Customer.java
@@ -0,0 +1,73 @@
+package cmu.csdetector.dummy.group;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Customer {
+    protected String name = "no name yet";
+    private List<Rental> rentals = new ArrayList<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator<Rental> rentals = this.rentals.iterator();
+        String result = "Rental Record for " + name + "\n";
+        
+        while (rentals.hasNext()) {
+            double thisAmount = 0;
+            Rental each = rentals.next();
+
+            //determine amounts for each line
+            switch (each.getTape().getMovie().priceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.daysRented() > 2)
+                        thisAmount += (each.daysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.daysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5;
+                    if (each.daysRented() > 3)
+                        thisAmount += (each.daysRented() - 3) * 1.5;
+                    break;
+
+            }
+            totalAmount += thisAmount;
+
+            // add frequent renter points
+            frequentRenterPoints ++;
+
+            // add bonus for a two-day new release rental
+            if ((each.getTape().getMovie().priceCode() == Movie.NEW_RELEASE) && each.daysRented() > 1) frequentRenterPoints ++;
+
+            //show figures for this rental
+            result += "\t" + each.getTape().getMovie().getName()+ "\t" + thisAmount + "\n";
+
+        }
+        //add footer lines
+        result +=  "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+
+        return result;
+
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public static Customer get(String name) {
+        return Registrar.getCustomer(name);
+    }
+
+    public void persist() {
+        Registrar.addCustomer(this.name, this);
+    }
+}
