49f97b5ca8e091c32fa09c76c0a5b1221514f381
Sankalp Gunturi <sgunturi@cmu.edu>
2023-04-25 00:28:20 -0700

    Add getBestCluster in ExtractRefactoring class

--- src/main/java/cmu/csdetector/jqual/refactoringOperations/ExtractRefactoring.java
+++ src/main/java/cmu/csdetector/jqual/refactoringOperations/ExtractRefactoring.java
@@ -1,7 +1,19 @@
 package cmu.csdetector.jqual.refactoringOperations;
 
-public class ExtractRefactoring extends RefactoringOperation{
-    public ExtractRefactoring(){
+public class ExtractRefactoring extends RefactoringOperation {
 
+
+    public ExtractRefactoring () {
+    }
+
+    public Cluster getBestCluster () {
+        SortedMap<Integer, HashSet<String>> table = getHashMapForClustering();
+        Map<String, ASTNode> stringASTNodeMap = getStringASTNodeMap();
+        Map<ASTNode, Integer> declaredVars = extractVariableDeclarations(); 
+        ClusterManager cm = new ClusterManager(table, stringASTNodeMap, declaredVars); 
+        Set<Cluster> blocks = getGrabManifestsBlock();
+        Cluster recommendedCluster = cm.getBestCluster(blocks);      
+
+        return recommendedCluster;
     }
 }
